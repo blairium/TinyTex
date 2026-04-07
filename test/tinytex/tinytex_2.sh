df25263669650b459136c5aa42b985f7d695d9f5
Blair Haydon <71422563+blairium@users.noreply.github.com>
2026-04-07 12:18:35 +1000

    Update tinytex_2.sh

--- test/tinytex/tinytex_2.sh
+++ test/tinytex/tinytex_2.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "check pdflatex version" bash -c "PATH=$PATH:$HOME/bin pdflatex --version"
+
 check "check tlmgr version" bash -c "PATH=$PATH:$HOME/bin tlmgr --version"
 
 # Report result
